a6e965177d216678dbc3fd0c5945c7df4954008f
Christian Liebel <christian.liebel@thinktecture.com>
2022-01-31 15:44:54 +0100

    Lab #8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-pwa';
+
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.available.subscribe(() => {
+      if (confirm('Update verfügbar! Jetzt neu laden?')) {
+        location.reload();
+      }
+    });
+  }
 }
